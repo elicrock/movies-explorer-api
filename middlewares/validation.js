c8dc58e3be3cd458c64ba24845c70fe3aa43d5bd
Paul <paul.ivanov1990@gmail.com>
2023-09-09 01:57:37 +0300

    Some fix

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -6,7 +6,7 @@ const validateCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required(),
   }),
 });
 
